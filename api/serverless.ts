d58ef448a9fad35bd3c9515d4a7c3891ed46983e
AVAHC4 <118993169+AVAHC4@users.noreply.github.com>
2025-03-28 00:35:37 +0530

    Simplify API to work around Vercel serverless function limits

--- api/serverless.ts
+++
@@ -1,5 +0,0 @@
-// This file serves as a single entry point for all API routes in Vercel
-import { app } from './index';
-
-// Export a handler compatible with Vercel serverless functions
-export default app; 
