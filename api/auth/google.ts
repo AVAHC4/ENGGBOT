f1a680e35ffc9c9a524ac7bbe48a91fd261a6059
AVAHC4 <akhilchava4@gmail.com>
2025-07-27 22:07:24 +0530

    google_auth fixed

--- api/auth/google.ts
+++ api/auth/google.ts
@@ -105,7 +105,7 @@ passport.use(
       clientSecret: CLIENT_SECRET,
       callbackURL: CALLBACK_URL,
       scope: ["profile", "email"],
-      proxy: process.env.NODE_ENV === 'productin',
+      proxy: process.env.NODE_ENV === 'production',
       passReqToCallback: true,
       skipUserProfile: false,
       userProfileURL: "https://www.googleapis.com/oauth2/v2/userinfo"
