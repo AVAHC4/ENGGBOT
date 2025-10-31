e887c27adede8b255651f18004809d09db4c291e
AVAHC4 <akhilchava4@gmail.com>
2025-10-31 21:25:52 +0530

    Update UI components and interfaces

--- AI_UI/src/components/ui/popover.tsx
+++ AI_UI/src/components/ui/popover.tsx
@@ -5,7 +5,7 @@ import * as PopoverPrimitive from "@radix-ui/react-popover"
 
 import { cn } from "@/lib/utils"
 
-cnst Popover = PopoverPrimitive.Root
+const Popover = PopoverPrimitive.Root
 
 const PopoverTrigger = PopoverPrimitive.Trigger
 
