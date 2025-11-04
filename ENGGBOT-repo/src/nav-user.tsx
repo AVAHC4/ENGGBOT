2ce0b9d49b5ce4469dd3d01bc45396de6a9b5965
AVAHC4 <akhilchava4@gmail.com>
2025-11-04 21:30:38 +0530

    Update TypeScript/JavaScript code

--- ENGGBOT-repo/src/nav-user.tsx
+++ ENGGBOT-repo/src/nav-user.tsx
@@ -6,7 +6,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdnMenuGroup,
+  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
