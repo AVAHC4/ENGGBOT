27544aeba8458ec0828609a81f68f5a655a7f8cf
AVAHC4 <akhilchava4@gmail.com>
2025-11-08 21:49:07 +0530

    Update UI components and interfaces

--- AI_UI/src/components/code-editor.tsx
+++ AI_UI/src/components/code-editor.tsx
@@ -8,7 +8,7 @@ import 'prismjs/themes/prism-tomorrow.css';
 // Import languages
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-jsx';
-import 'prismjs/components/prism-typescript';
+import 'prismjs/coonents/prism-typescript';
 import 'prismjs/components/prism-tsx';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-markup'; // HTML
