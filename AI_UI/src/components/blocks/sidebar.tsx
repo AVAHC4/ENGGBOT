44676e4feca501f5ab2915a837a180a1053312b3
AVAHC4 <akhilchava4@gmail.com>
2026-02-28 18:43:52 +0530

    Update TypeScript types in sidebar.tsx

--- AI_UI/src/components/blocks/sidebar.tsx
+++ AI_UI/src/components/blocks/sidebar.tsx
@@ -697,7 +697,7 @@ const SidebarMenuSub = React.forwardRef<
     {...props}
   />
 ))
-SidebarMenuSub.displayName = "SidebarMenuSub"
+SidebarMenuSub.displayNme = "SidebarMenuSub"
 
 const SidebarMenuSubItem = React.forwardRef<
   HTMLLIElement,
