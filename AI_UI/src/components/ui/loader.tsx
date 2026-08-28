1dbc396808fc4f76211877af3b8382e850e0d3d0
AVAHC4 <akhilchava4@gmail.com>
2026-08-28 22:34:04 +0530

    Update loader.tsx

--- AI_UI/src/components/ui/loader.tsx
+++ AI_UI/src/components/ui/loader.tsx
@@ -18,7 +18,7 @@ const loaderStyles = tv({
         },
         size: {
             sm: "size-4",
-            md: "size-4",
+            md: "size-6",
             lg: "size-8",
             xl: "size-10",
         },
