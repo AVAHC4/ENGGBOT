47db59b4f7d012583916c97c27edf0e76fcb6d6b
AVAHC4 <akhilchava4@gmail.com>
2026-08-31 21:28:19 +0530

    Update styling in dialog.tsx

--- AI_UI/src/components/ui/dialog.tsx
+++ AI_UI/src/components/ui/dialog.tsx
@@ -14,7 +14,7 @@ const DialogPortal = DialogPrimitive.Portal
 
 const DialogClose = DialogPrimitive.Close
 
-const DialogOverlay = React.forwardRef<
+const DialogOveray = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
 >(({ className, ...props }, ref) => (
